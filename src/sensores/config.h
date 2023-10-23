4899c1190f929090d0f6c984e84ad4ed6dd3bfdc
Henrique <henrique.azr@outlook.com>
2023-10-23 18:59:45 -0300

    conexão com a rede wifi e plataforma adafruitIO

---
+++ src/sensores/config.h
@@ -0,0 +1,21 @@
+// config.h
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Configuração da conexão WiFi
+#define NOME_WIFI "nome rede wifi"
+#define SENHA_WIFI "senha do wifi"
+
+// Configuração do Adafruit IO
+#define AIO_USERNAME "username"
+#define AIO_KEY "chave de acesso"
+
+// FEEDS
+#define DHT11_TEMPERATURA "/feeds/pi-5.dht11-temperatura"
+#define DHT11_UMIDADE "/feeds/pi-5.dht11-umidade"
+#define CS12_UMIDADE "/feeds/pi-5.cs12-umidade"
+#define DSB18B20_TEMPERATURA "/feeds/pi-5.ds18b20-temperatura"
+#define BH1750_LUX "/feeds/pi-5.bh1750-luminosidade"
+
+#endif // CONFIG_H
--- src/sensores/sensores.ino
+++ src/sensores/sensores.ino
@@ -1,3 +1,6 @@
+#include <Adafruit_MQTT.h>
+#include <Adafruit_MQTT_Client.h>
+#include <ESP8266WiFi.h>
 #include <DHTesp.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
@@ -5,16 +8,27 @@
 #include <BH1750.h>
 #include "config.h"
 
-//Portas dos sensores
+// Portas dos sensores
 #define PortaUmidadeSolo = A0
 #define PortaDHT = 14
 #define PortaDS18B20 = D7
 
-//Instanciando objetos para os sensores
+// Instanciando objetos para os sensores
 DHTesp dht;
 OneWire barramento(PortaDS18B20);
 DallasTemperature sensorDS18B20(&barramento);
 BH1750 sensorBH1750;
+
+// Objeto para conexão com a Adafruit IO via MQTT
+WiFiClient client;
+Adafruit_MQTT_Client mqtt(&client, "io.adafruit.com", 1883, AIO_USERNAME, AIO_KEY);
+
+// Objetos para conexão com os feeds dos sensores
+Adafruit_MQTT_Publish cs12Umidade = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME CS12_UMIDADE);
+Adafruit_MQTT_Publish dht11Temperatura = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME DHT11_TEMPERATURA);
+Adafruit_MQTT_Publish dht11Umidade = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME DHT11_UMIDADE);
+Adafruit_MQTT_Publish ds18b2Temperatura = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME DSB18B20_TEMPERATURA);
+Adafruit_MQTT_Publish bh1750lux = Adafruit_MQTT_Publish(&mqtt, AIO_USERNAME BH1750_LUX);
 
 void setup() {
   
@@ -24,43 +38,83 @@ void setup() {
   // DS18B20
   dht.setup(PortaDHT, DHTesp::DHT11);
 
-  //DS18B20
+  // DS18B20
   sensor.begin();
 
   // BH1750
   Wire.begin();
   lightMeter.begin();
+
+  //Conexão com a rede WiFi
+  Serial.println("Conectando ao WiFi...");
+  WiFi.begin(NOME_WIFI, SENHA_WIFI);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.println("Conectado ao WiFi!");
 }
 
 void loop() {
 
+  // Iniciando a conexão com a plataforma AdafruitIO
+  MQTTconnect();
+
   // Umidade Solo
-  int umidadeSolo = analogRead(PortaUmidadeSolo);
+  int valorUmidadeSolo = analogRead(PortaUmidadeSolo);
   Serial.print("Umidade do Solo: ");
-  Serial.print(umidadeSolo);
+  Serial.print(valorUmidadeSolo);
   Serial.println(" %");
   
   // DHT11
-  float temperaturaDHT = dht.getTemperature();
-  float umidadeDHT = dht.getHumidity();
+  float valorTemperaturaDHT = dht.getTemperature();
+  float valorUmidadeDHT = dht.getHumidity();
   Serial.print("Temperature: ");
-  Serial.print(temperaturaDHT);
+  Serial.print(valorTemperaturaDHT);
   Serial.println(" °C");
   Serial.print("Humidity: ");
-  Serial.print(umidadeDHT);
+  Serial.print(valorUmidadeDHT);
   Serial.println(" %");
 
-  //BH1750
-  float luxBH1750 = sensorBH1750.readLightLevel();
-
-  //DS18B20
+  // DS18B20
   sensorDS18B20.requestTemperatures();
-  Serial.println(sensorBH1750.getTempCByIndex(0));
-  
-  //GY-30
+  int valorBH1750temperatura = sensorBH1750.getTempCByIndex(0);
+  Serial.println(valorBH1750temperatura);
+
+  // BH1750
+  float valorLuxBH1750 = sensorBH1750.readLightLevel();
   Serial.print("Light: ");
-  Serial.print(luxBH1750);
+  Serial.print(valorLuxBH1750);
   Serial.println(" lx");
+  
+  // Publicando dados dos sensores
+  cs12Humidade.publish(valorUmidadeSolo);
+  dht11Temperatura.publish(valorTemperaturaDHT);
+  dht11Umidade.publish(valorUmidadeDHT);
+  ds18b2Temperatura.publish(valorBH1750temperatura);
+  bh1750lux.publish(valorLuxBH1750);
 
   delay(5000);
+}
+
+
+void MQTTconnect() {
+  int8_t ret;
+
+  // Verifica se já está conectado
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Conectando ao Adafruit IO...");
+
+  while ((ret = mqtt.connect()) != 0) {
+    Serial.println(mqtt.connectErrorString(ret));
+    Serial.println("Tentando novamente em 5 segundos...");
+    mqtt.disconnect();
+    delay(5000);
+  }
+
+  Serial.println("Conectado!");
 }
